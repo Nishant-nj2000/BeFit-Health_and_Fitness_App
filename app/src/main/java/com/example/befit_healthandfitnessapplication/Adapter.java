37d44695ee215de4d27d458d3444f9a7897c3aac
Nishant Jain <nishant.nj2000@gmail.com>
2021-09-26 02:03:51 +0530

    homepage redesigned

--- app/src/main/java/com/example/befit_healthandfitnessapplication/Adapter.java
+++
@@ -1,4 +0,0 @@
-package com.example.befit_healthandfitnessapplication;
-
-public class Adapter {
-}
--- app/src/main/java/com/example/befit_healthandfitnessapplication/DBHelper.java
+++ app/src/main/java/com/example/befit_healthandfitnessapplication/DBHelper.java
@@ -13,9 +13,11 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "befit.db";
     public static final String TABLE_NAME = "user";
     public static final String COL_1 = "userid";
-    public static final String COL_2 = "email";
-    public static final String COL_3 = "gender";
-    public static final String COL_4 = "password";
+    public static final String COL_2 = "fullname";
+    public static final String COL_3 = "email";
+    public static final String COL_4 = "gender";
+    public static final String COL_5 = "password";
+
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -31,10 +33,11 @@ public class DBHelper extends SQLiteOpenHelper {
 
     }
 
-    public long addUser(String email, String gender, String password)
+    public long addUser(String fullname, String email, String gender, String password)
     {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
+        contentValues.put("fullname",fullname);
         contentValues.put("email",email);
         contentValues.put("gender",gender);
         contentValues.put("password",password);
@@ -46,7 +49,7 @@ public class DBHelper extends SQLiteOpenHelper {
     public boolean checkUser(String email, String password){
         String[] columns = { COL_1 };
         SQLiteDatabase db = getReadableDatabase();
-        String selection = COL_2 + "=?" + " and "+ COL_4 + "=?";
+        String selection = COL_3 + "=?" + " and "+ COL_5 + "=?";
         String[] selectionArgs = {email,password};
         Cursor cursor = db.query(TABLE_NAME,columns,selection,selectionArgs,null,null,null);
         int count = cursor.getCount();
